8084cd806497ddc521f63628a203631cec07a6e6
Shiraz Suleman <shirazsuleman@gmail.com>
2019-09-21 17:00:04 +0200

    Updated script to generate csv file and use asyncio

--- rugby/get_irb_top5_rankings.py
+++ rugby/get_irb_top5_rankings.py
@@ -1,39 +1,52 @@
 import requests
-from datetime import date, timedelta
+import datetime
 import json
+import csv
+import aiohttp
+import asyncio
 
 URL = 'https://cmsapi.pulselive.com/rugby/rankings/mru?date={}&client=pulse'
-
-IRB_RANKINGS = []
+CSV_FILE_NAME = 'irb_rankings.csv'
 
 def all_mondays(year):
-   d = date(year, 1, 1)                    # January 1st
-   d += timedelta(days = (d.weekday() if d.weekday() == 0 else 7 - d.weekday()))  # First Monday
+   d = datetime.date(year, 1, 1)                    # January 1st
+   d += datetime.timedelta(days = (d.weekday() if d.weekday() == 0 else 7 - d.weekday()))  # First Monday
    while d.year == year:
       yield d
-      d += timedelta(days = 7)
+      d += datetime.timedelta(days = 7)
 
-def main():
-    for d in all_mondays(2019):
-        if d > date.today():
-            break
+def create_csv_file():
+    with open(CSV_FILE_NAME, mode='w') as irb_rankings_file:
+        writer = csv.writer(irb_rankings_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['Date', 'Team', 'Rank', 'Points'])
 
-        response = requests.get(URL.format(str(d)))
-        entries = json.loads(response.text)['entries']
+def add_csv_row(date, rank, team, points):
+    with open(CSV_FILE_NAME, mode='a') as irb_rankings_file:
+        writer = csv.writer(irb_rankings_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow([date, team, rank, points])
 
-        period = dict()
-        period['Date'] = str(d)
-        period['Rankings'] = []
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
-        for i in range(5):
-            period['Rankings'].append({
-                "Team": entries[i]['team']['name'],
-                "Position": i + 1,
-                "Points": round(entries[i]['pts'], 2)
-            })
-        
-        IRB_RANKINGS.append(period)
-    print(IRB_RANKINGS)
+async def process_date(session, date):
+    if date > date.today() or date < datetime.date(2003, 10, 13):
+        return
+
+    response = await fetch(session, URL.format(str(date)))
+    entries = json.loads(response)['entries']
+
+    for i in range(5):
+        entry = entries[i]
+        add_csv_row(date, i + 1, entry['team']['name'], round(entry['pts'], 2))
+
+async def main():
+    create_csv_file()
+    years = [2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+    for year in years:
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+            await asyncio.gather(*(process_date(session, d) for d in all_mondays(year)))
 
 if __name__ == '__main__':
-    main()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
