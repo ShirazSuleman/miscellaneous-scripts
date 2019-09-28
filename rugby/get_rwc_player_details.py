9e5ca2773309990e8173bf8bebf6f3a86e70e02e
Shiraz Suleman <shirazsuleman@gmail.com>
2019-09-28 21:01:21 +0200

    Made changes to get teams.

--- rugby/get_rwc_player_details.py
+++ rugby/get_rwc_player_details.py
@@ -4,6 +4,7 @@ import aiohttp
 import asyncio
 import requests
 from bs4 import BeautifulSoup
+import os
 
 CSV_FILE_NAME = 'rwc_players.csv'
 
@@ -15,17 +16,19 @@ async def get_soup(session, url):
 async def get_players(url):
     players = []
 
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+    async with aiohttp.ClientSession() as session:
         soup = await get_soup(session, url)
 
     tables = soup.find_all('table', class_="sortable wikitable")
 
     for table in tables:
+        team = table.parent.parent.parent.parent.find_previous_sibling('h3').find('span', class_='mw-headline').get_text()
         rows = table.find_all('tr', class_="vcard agent")
         for row in rows:
             attrs = row.select('td span.fn > a')[0].attrs
-            player = { 'url': 'https://en.m.wikipedia.org' + attrs['href'], 'name': attrs['title']}
-            players.append(player)
+            if 'Captain (sports)' not in attrs['title']:
+                player = { 'url': 'https://en.m.wikipedia.org' + attrs['href'], 'name': attrs['title'], 'team': team}
+                players.append(player)
 
     return players
 
@@ -36,22 +39,25 @@ async def get_player_location(session, player_url):
 
     table = soup.find('table', class_='infobox vcard')
 
-    location_elements = table.find('th', text = re.compile('place.*birth', re.IGNORECASE)).next_sibling()
+    location_elements = table.find('th', text = re.compile('place.*birth', re.IGNORECASE)).next_sibling
 
     for element in location_elements:
-        player_location = player_location + (element.get_text() + ', ')
+        try:
+            player_location = player_location + (element.get_text() + ', ')
+        except Exception as e:
+            print(e)
 
     return player_location[0:-2]
 
-def create_csv_file():
-    with open(CSV_FILE_NAME, mode='w') as rwc_players_file:
+def new_csv_file():
+    with open(CSV_FILE_NAME, mode='w', encoding='utf-8', newline='') as rwc_players_file:
         writer = csv.writer(rwc_players_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['Name', 'URL', 'Born'])
+        writer.writerow(['Name', 'URL', 'Born', 'Team'])
 
 def add_csv_row(player):
-    with open(CSV_FILE_NAME, mode='a') as rwc_players_file:
+    with open(CSV_FILE_NAME, mode='a', encoding='utf-8', newline='') as rwc_players_file:
         writer = csv.writer(rwc_players_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([player['name'], player['url'], player['born']])
+        writer.writerow([player['name'], player['url'], player['born'], player['team']])
 
 async def process_player(session, player):
     try:
@@ -63,15 +69,14 @@ async def process_player(session, player):
         add_csv_row(player)
 
 async def main():
-    url = 'https://en.m.wikipedia.org/wiki/2019_Rugby_World_Cup_squads'
+    new_csv_file()
 
-    create_csv_file()
+    url = 'https://en.m.wikipedia.org/wiki/2019_Rugby_World_Cup_squads'
 
     players = await get_players(url)
 
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+    async with aiohttp.ClientSession() as session:
         await asyncio.gather(*(process_player(session, p) for p in players))
-
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
